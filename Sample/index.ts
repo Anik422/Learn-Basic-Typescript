8544acbd76821c9b85160e6e093ae2fbb75e48fe
anik <aniks422@gmail.com>
2024-12-02 14:38:08 +0600

    Section 2: Getting Started with the Basics

---
+++ Sample/index.ts
@@ -0,0 +1,30 @@
+// function greet(name:string){
+//     console.log(`Hello, ${name}!`);
+// }
+
+// greet('World');
+
+
+// let message: string = "Hello, TypeScript!";
+// console.log(message);
+
+
+// let fastName: string = "Jane";
+// let age: number = 30;
+// let isStudent: boolean = true;
+// let empty: null = null;
+// let und: undefined = undefined;
+
+// let notSure: any = 4;
+// notSure = "maybe a string instead";
+// notSure = false;
+
+
+
+function add(x: number, y: number): number {
+    return x + y;
+}
+console.log(add(1, 2)); 
+
+
+let p: string|number = 10;
